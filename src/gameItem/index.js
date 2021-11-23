dbe65f7a9a1aa28ebb0f5164af05ecc93ab5a95b
Pavankumarvinukonda <vinukondapavankumar1@gmail.com>
2021-11-23 23:02:47 +0000

    test case errors but functionlity works

--- src/gameItem/index.js
+++ src/gameItem/index.js
@@ -1,45 +1,23 @@
 import './index.css'
 
 const GameItem = props => {
-  const {choicesList, changeActiveImage} = props
-
-  const clikingImg = choicesList => {
-    changeActiveImage(choicesList)
+  const {gameDetails, changeActiveImage, testIds} = props
+  const {id, imageUrl} = gameDetails
+  const clikingImg = () => {
+    changeActiveImage(id, imageUrl)
   }
 
   return (
-    <>
-      <li key={choicesList[0].id} className="listItem">
-        <button data-testid="rockButton" className="buttons" type="button">
-          <img
-            onClick={clikingImg(choicesList[0])}
-            src={choicesList[0].imageUrl}
-            alt="rpsImaes"
-            className="GameImg"
-          />
-        </button>
-      </li>
-      <li key={choicesList[1].id} className="listItem">
-        <button data-testid="paperButton" className="buttons" type="button">
-          <img
-            onClick={clikingImg[choicesList[1]]}
-            src={choicesList[1].imageUrl}
-            alt="rpsImaes"
-            className="GameImg"
-          />
-        </button>
-      </li>
-      <li key={choicesList[2].id} className="listItem">
-        <button data-testid="scissorsButton" className="buttons" type="button">
-          <img
-            onClick={clikingImg(choicesList[2])}
-            src={choicesList[2].imageUrl}
-            alt="rpsImaes"
-            className="GameImg"
-          />
-        </button>
-      </li>
-    </>
+    <li className="listItem">
+      <button className="buttons" type="button">
+        <img
+          onClick={clikingImg}
+          src={imageUrl}
+          alt="rpsImaes"
+          className="GameImg"
+        />
+      </button>
+    </li>
   )
 }
 
